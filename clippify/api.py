c14973763c0dc36948a687310bc9a59288d2e023
Timothy Crosley <timothy.crosley@gmail.com>
2015-10-13 18:52:37 -0700

    Add initial API for retrieving and setting notes

--- clippify/api.py
+++ clippify/api.py
@@ -1,17 +1,23 @@
 import hug
 import plyvel
+import arrow
+from datetime import datetime
+
+
+def database(user_id):
+    '''Returns the level db unique to the given user_name'''
+    return plyvel.DB('/clippify/users/{user_id}'.format(user_id=user_id), create_if_missing=True)
 
 
 @hug.get()
-def get_notes(user):
-    return
+def get_notes(user:database):
+    '''Returns all notes for the specified user'''
+    return {date: note for date, note in user}
 
 
-@hug.post()
-def update_note(user, note):
-    return
-
-
-@hug.post()
-def save_note(user, note):
-    return
+@hug.post(('/{user}/note/', '/{user}/note/{timestamp}'))
+def save_note(user:database, note, timestamp:arrow.get=None):
+    '''Saves a note in the specified user database, or updates an existing one with a set timestamp'''
+    timestamp = timestamp or arrow.get()
+    user.put(bytes(timestamp), bytes(note))
+    return timestamp
