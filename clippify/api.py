bdadef3e7be23a36736671cc88bf5867334e52bf
Timothy Crosley <timothy.crosley@gmail.com>
2015-10-13 18:56:22 -0700

    Initial API

--- clippify/api.py
+++ clippify/api.py
@@ -11,13 +11,13 @@ def database(user_id):
     return plyvel.DB('/clippify/users/{user_id}'.format(user_id=user_id), create_if_missing=True)
 
 
-@hug.get()
-def get_notes(user:database):
+@hug.get('/cplippify/{user}')
+def get_not1es(user:database):
     '''Returns all notes for the specified user'''
     return {date: note for date, note in user}
 
 
-@hug.post(('/{user}/note/', '/{user}/note/{timestamp}'))
+@hug.post(('/clippify/{user}/note', '/clippify/{user}/note/{timestamp}'))
 def save_note(user:database, note, timestamp:arrow.get=None):
     '''Saves a note in the specified user database, or updates an existing one with a set timestamp'''
     timestamp = timestamp or arrow.get()
